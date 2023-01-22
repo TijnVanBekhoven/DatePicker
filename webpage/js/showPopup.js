92494b6a85d32c285b26b7cae5e1b44d459c7568
treinio <tijnvbek@gmail.com>
2023-01-22 12:14:13 +0100

    Fixed popup screen bug

--- webpage/js/showPopup.js
+++ webpage/js/showPopup.js
@@ -1,5 +1,5 @@
 function showPopup() {
-    const popupScreen = document.getElementById("loginPopup");
+    const popupScreen = document.getElementById("popup");
     const body = document.querySelector("body");
     const container = document.getElementById("container");
 
@@ -10,7 +10,7 @@ function showPopup() {
 }
 
 function closePopup() {
-    const popupScreen = document.getElementById("loginPopup");
+    const popupScreen = document.getElementById("popup");
     const body = document.querySelector("body");
     const container = document.getElementById("container");
 
